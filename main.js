f9d8186db59b543fe276d8a190e54de823738b1d
abhi3315 <abhishek.sharma@rtcamp.com>
2022-01-27 14:20:08 +0530

    update

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var sdk = new window.sfdc.BlockSDK();
 
 sdk.setContent(
-	'<div data-type="slot" data-key="450bk17qpxc" data-allowed-blocks="" data-max-blocks="" data-label="Drop blocks or content here"></div>'
+	'<p>jjj</p>',
 );
