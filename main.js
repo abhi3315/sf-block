8d9ac1fcdd46cdf0047107eccf5dbbfe5f92e33c
abhi3315 <abhishek.sharma@rtcamp.com>
2022-01-27 13:33:05 +0530

    add custom content

---
+++ main.js
@@ -0,0 +1,5 @@
+var sdk = new window.sfdc.BlockSDK();
+
+sdk.setContent(
+	'<div data-type="slot" data-key="450bk17qpxc" data-allowed-blocks="" data-max-blocks="" data-label="Drop blocks or content here"></div>'
+);
